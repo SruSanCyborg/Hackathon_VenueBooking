5c5546d79e27d55a862601d7d8ae7fc84b699a92
Sivakumar Dhakshinamoorthy <dsivakumar@Lenovo.com>
2024-09-27 00:15:07 +0530

    Login

---
+++ Signup/validation.js
@@ -0,0 +1,103 @@
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+  let errors = []
+  if(firstname_input){
+    errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
+  }
+  else{
+      errors = getLoginFormErrors(email_input.value, password_input.value)
+  }
+
+  if(errors.length > 0){
+    e.preventDefault()
+    error_message.innerText  = errors.join(". ")
+  }
+})
+
+function getSignupFormErrors(firstname, email, password, repeatPassword){
+  let errors = []
+
+  if(firstname === '' || firstname == null){
+    errors.push('Firstname is required')
+    firstname_input.parentElement.classList.add('incorrect')
+  }
+  if(email === '' || email == null){
+    errors.push('Email is required')
+    email_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password.length < 8){
+    errors.push('Password must have at least 8 characters')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password !== repeatPassword){
+    errors.push('Password does not match repeated password')
+    password_input.parentElement.classList.add('incorrect')
+    repeat_password_input.parentElement.classList.add('incorrect')
+  }
+
+
+  return errors;
+}
+
+function getLoginFormErrors(email, password){
+  let errors = []
+
+  if(email === '' || email == null){
+    errors.push('Email is required')
+    email_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+
+  return errors;
+}
+
+const allInputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
+
+allInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect')
+      error_message.innerText = ''
+    }
+  })
+})
+
+
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const username = form.username.value
+    const password = form.password.value
+
+    const authenticated = authentication(username,password)
+
+    if(authenticated){
+        window.location.href = "logout.html"
+    }else{
+        alert("wrong")
+    }
+})
+
+// function for checking username and password
+
+function authentication(username,password){
+    if(username === "student" && password === "password"){
+        return true
+    }else{
+        return false
+    }
+}
